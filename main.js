5bf51e43967abdf44e3eada150ab02a77d66b076
AstakhovV <astakhclever@gmail.com>
2021-07-06 17:27:46 +0500

    Portfolio

--- main.js
+++ main.js
@@ -57,3 +57,18 @@ tabs.forEach(tab => {
         tab.classList.add('qualification_active')
     })
 })
+
+//swiper
+let swiper = new Swiper(".portfolio_container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    keyboard: true,
+});
